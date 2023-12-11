5ae6fcc417e3be0fe7daad8824f69fb95f609bd6
kahlilbausley <42008951+kahlilbausley@users.noreply.github.com>
2023-12-11 13:07:30 -0800

    finished product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      autoIncrement:true,
+      primaryKey:true
+    },
+    product_name:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    stock:{
+      type:DataTypes.INTEGER,
+      defaultValue:10,
+      allowNull:false
+    },
+    price:{
+      type:DataTypes.DECIMAL,
+      allowNull:false
+    },
+    category_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:"category",
+        key:"id"
+      }
+    }
   },
   {
     sequelize,
