d620e481052636507a13c21457deafab792c4aa9
kahlilbausley <42008951+kahlilbausley@users.noreply.github.com>
2023-12-21 12:28:41 -0800

    finished models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,25 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      autoIncrement:true,
+      primaryKey:true
+    },
+    product_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:"product",
+        key:"id"
+      }
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:"category",
+        key:"id"
+      }
+    }
   },
   {
     sequelize,
